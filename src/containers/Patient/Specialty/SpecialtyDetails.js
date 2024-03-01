4116abba4e4bbd38a97702a47285d7e2d2bd1ef5
oanhvlt <ovu1024@gmail.com>
2024-03-01 22:51:31 +0700

    SpecialtyDetails

--- src/containers/Patient/Specialty/SpecialtyDetails.js
+++ src/containers/Patient/Specialty/SpecialtyDetails.js
@@ -1,20 +1,23 @@
 //SpecialtyDetails
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './SpecialtyDetails.scss';
-import HomeMenu from '../../HomePage/HomeHeader/HomeMenu';
-import '../../HomePage/HomePage.scss';
-
 // import { FormattedMessage } from 'react-intl';
 // import { LANGUAGES } from '../../../utils';
 // import {getScheduleByDateService} from '../../../services/userService';
+import './SpecialtyDetails.scss';
+import HomeMenu from '../../HomePage/HomeHeader/HomeMenu';
+import '../../HomePage/HomePage.scss';
+import DoctorSchedule from '../Doctor/DoctorSchedule';
+import DoctorExtraInfo from '../Doctor/DoctorExtraInfo';
+import ProfileDoctor from '../Doctor/ProfileDoctor';
+
 
 class SpecialtyDetails extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-         
+            arrDoctorId:[42,44,45]
         }
     }
 
@@ -32,9 +35,43 @@ class SpecialtyDetails extends Component {
 
 
     render() {
+        let {arrDoctorId} = this.state;
         return ( 
             <div className='container-page'>
                 <HomeMenu />
+                <div className='specialty-details-container'>
+                    <div className='description'>
+
+                    </div>
+
+                    {arrDoctorId && arrDoctorId.length > 0 &&
+                        arrDoctorId.map((item, index) => {
+                            return(
+                                <div className='eachDotor' key={index} >
+                                    <div className='dt-content-left'>
+                                        <div className='profile-doctor'>
+                                            <ProfileDoctor
+                                            doctorIdFromParent = {item}
+                                            isShowDescriptionDoctor = {true}
+                                            //dataTime = {dataTime}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className='dt-content-right'>
+                                        <div className='schedule'>
+                                            <DoctorSchedule doctorIdFromParent= {item}/>
+                                        </div>
+                                        <div className='extra-info'>
+                                            <DoctorExtraInfo doctorIdFromParent= {item}/>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            )
+                        })
+                    }
+                </div>
+               
             </div>
         );
     }
